e1a858c19b0ca63a4149d8f4eb1bfe2e8ab3d386
ArnaudJalbert <74669399+ArnaudJalbert@users.noreply.github.com>
2023-10-12 17:50:48 -0400

    particles done and visualization

--- exercise_one/public/js/sketch.js
+++ exercise_one/public/js/sketch.js
@@ -1,8 +1,8 @@
-let system;
+let particleSystem;
 let slider;
-
 let currentYear;
 let F1DriversActiveYears;
+let particleSize;
 
 async function getF1DriversActiveYears(year) {
   let F1DriversActiveYearsResponse = await fetch(
@@ -20,88 +20,147 @@ async function setup() {
   // canvas init
   createCanvas(window.innerWidth, window.innerHeight);
 
+  // keeping track of the visualized year
   currentYear = slider.value();
-  F1DriversActiveYears = await getF1DriversActiveYears(currentYear);
 
-  // particle system
-  system = new ParticleSystem(
-    createVector(window.innerWidth / 2, window.innerHeight / 2),
+  // particles setup
+  particleSystem = new ParticleSystem();
+  particleSize = 10;
+
+  // setting the initional particles
+  await particleSystem.setParticles(currentYear);
+
+  slider.elt.addEventListener("mouseup", () => {
+    particleSystem.setParticles();
+  });
+}
+
+async function draw() {
+  // background setup
+  background(200);
+  particleSystem.show();
+
+  fill("black");
+  strokeWeight(0);
+  textSize(25);
+  text(
+    `Formula 1 Drivers Active for at least ${currentYear} year(s)`,
+    10,
+    window.innerHeight - 15,
+  );
+  fill("#F18F01");
+  ellipse((13 * window.innerWidth) / 32, window.innerHeight - 20, 20);
+  fill("#000000");
+  textSize(20);
+  text(
+    "= F1 Champion",
+    (13 * window.innerWidth) / 32 + 10,
+    window.innerHeight - 15,
+  );
+  fill("#EF959D");
+  ellipse((17 * window.innerWidth) / 32, window.innerHeight - 20, 20);
+  fill("#000000");
+  textSize(20);
+  text(
+    "= Pole Sitter+Winner",
+    (17 * window.innerWidth) / 32 + 10,
+    window.innerHeight - 15,
+  );
+  fill("#99C24D");
+  ellipse((22 * window.innerWidth) / 32, window.innerHeight - 20, 20);
+  fill("#000000");
+  textSize(20);
+  text(
+      "= Winner",
+      (22 * window.innerWidth) / 32 + 10,
+      window.innerHeight - 15,
   );
 
-  for (const F1Driver of F1DriversActiveYears) {
-    system.addParticle();
-  }
+  fill("#41BBD9");
+  ellipse((24.5 * window.innerWidth) / 32, window.innerHeight - 20, 20);
+  fill("#000000");
+  textSize(20);
+  text(
+      "= Pole Sitter",
+      (24.5 * window.innerWidth) / 32 + 10,
+      window.innerHeight - 15,
+  );
+
+  fill("#555B6E");
+  ellipse((27.5 * window.innerWidth) / 32, window.innerHeight - 20, 20);
+  fill("#000000");
+  textSize(20);
+  text(
+      "= No Pole/Win",
+      (27.5 * window.innerWidth) / 32 + 10,
+      window.innerHeight - 15,
+  );
 }
 
 // inspired by: https://p5js.org/examples/simulate-particle-system.html
-
-async function draw() {
-  if (slider.value() !== currentYear) {
-    currentYear = slider.value();
-    F1DriversActiveYears = await getF1DriversActiveYears(currentYear);
-    system.clear();
-    for (const F1Driver of F1DriversActiveYears) {
-      system.addParticle();
-    }
+class Particle {
+  constructor(driver) {
+    this.x = Math.random() * (window.innerWidth - 40) + 20;
+    this.y = Math.random() * (window.innerHeight - 100) + 50;
+    this.driver = driver;
   }
-  background(100);
-  system.run();
+
+  show() {
+    // checking if they are a champion
+    if (this.driver.Champion === "True") {
+      fill("#F18F01");
+    }
+    // race winner and pole sitter
+    else if (this.driver.Race_Wins > 0 && this.driver.Pole_Positions > 0) {
+      fill("#EF959D");
+    } else if (this.driver.Race_Wins > 0) {
+      fill("#99C24D");
+    }
+    // checking if they are a pole sitter
+    else if (this.driver.Pole_Positions > 0) {
+      fill("#41BBD9");
+    } else {
+      fill("#555B6E");
+    }
+    ellipse(this.x, this.y, particleSize);
+  }
+
+  animate() {
+    this.x = this.x + (Math.random() * 0.4 - 0.2);
+    this.y = this.y + (Math.random() * 0.4 - 0.2);
+  }
 }
 
-let Particle = function (position) {
-  this.acceleration = createVector(0, 0); // Change y-component to negative for buoyant force
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
-  this.position = position.copy();
-  this.position.x = Math.random() * window.innerWidth;
-  this.position.y = Math.random() * window.innerHeight;
-  this.alive = true;
-};
+class ParticleSystem {
+  constructor() {
+    this.particles = Array();
+    this.F1Drivers = Array();
+  }
 
-Particle.prototype.run = function () {
-  this.update();
-  this.display();
-};
+  addParticle(F1Driver) {
+    this.particles.push(new Particle(F1Driver));
+  }
 
-// Method to update position
-Particle.prototype.update = function () {
-  this.velocity.add(this.acceleration);
-  this.position.add(this.velocity);
-};
+  clearParticles() {
+    this.particles = Array();
+  }
 
-// Method to display
-Particle.prototype.display = function () {
-  stroke(200, this.lifespan);
-  strokeWeight(2);
-  fill(127, this.lifespan);
-  ellipse(this.position.x, this.position.y, 12, 12);
-};
+  async setParticles() {
+    this.clearParticles();
+    currentYear = slider.value();
+    // get the f1 drivers data
+    this.F1Drivers = await getF1DriversActiveYears(currentYear);
+    for (const F1Driver of this.F1Drivers) {
+      particleSystem.addParticle(F1Driver);
+    }
+    particleSize = 838 / this.F1Drivers.length + 20;
+    console.log(particleSize);
+  }
 
-// Is the particle still useful?
-Particle.prototype.isDead = function () {
-  return this.lifespan < 0;
-};
-
-let ParticleSystem = function (position) {
-  this.origin = position.copy();
-  this.particles = [];
-};
-
-ParticleSystem.prototype.addParticle = function () {
-  this.particles.push(new Particle(this.origin));
-};
-
-ParticleSystem.prototype.run = function () {
-  for (let i = this.particles.length - 1; i >= 0; i--) {
-    let p = this.particles[i];
-    p.run();
-    if (p.isDead()) {
-      this.particles.splice(i, 1);
+  show() {
+    for (const particle of this.particles) {
+      particle.animate();
+      particle.show();
     }
   }
-};
-
-ParticleSystem.prototype.clear = function () {
-  for (let i = this.particles.length - 1; i >= 0; i--) {
-    this.particles[i].lifespan = 0;
-  }
-};
+}
